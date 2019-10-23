b52eea8a941ebfa8fb01c02dee13ba155297e64d
CPD <itapira.cpd@unip.br>
2019-10-23 11:14:56 -0300

    INITIAL COMMIT

---
+++ init.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install -y nodejs yarn gdebi
+wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi -n /tmp/chrome.deb
+wget -O /tmp/code.deb 
+sudo gdebi -n /tmp/chrome.deb
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code
+
+git config --global user.email "itapira.cpd@unip.br"
+git config --global user.name "uniesi"
